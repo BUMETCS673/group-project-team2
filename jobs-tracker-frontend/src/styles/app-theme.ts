f5ef779b56aa741e6642e4f76b8be080f3feb285
Taina Conde <tainabatistaconde@gmail.com>
2022-05-26 15:30:29 -0400

    Add DefaultTheme style

---
+++ jobs-tracker-frontend/src/styles/app-theme.ts
@@ -0,0 +1,31 @@
+import { DefaultTheme } from "styled-components";
+
+const appTheme: DefaultTheme = {
+  colors: {
+    primary: {
+      main: "#80D0C2",
+    },
+    secondary: {
+      main: "#EF9C4B",
+      dark: "#FD8030",
+    },
+    text: {
+      main: "#848080",
+      secondary: "#FFFFFF",
+    },
+    background: {
+      primary: "rgba(128, 208, 194, 0.1)",
+      secondary: "#F9F9F9",
+    }
+  },
+  input: {
+    borderRadius: "10px",
+    backgroundColor: {
+      primary: "#F8F8F8",
+      secondary: 'rgba(239, 223, 75, .23)',
+    },
+    opacity: 0.4,
+  },
+};
+
+export { appTheme };
