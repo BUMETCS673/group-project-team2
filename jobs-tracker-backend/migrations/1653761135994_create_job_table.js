aa181685571de9219f9570a98b203ba6cc3935c4
Bharat <bharatgoku@gmail.com>
2022-05-31 13:53:50 -0400

    Update Database Structure

--- jobs-tracker-backend/migrations/1653761135994_create_job_table.js
+++ jobs-tracker-backend/migrations/1653761135994_create_job_table.js
@@ -1,4 +1,4 @@
 module.exports = {
-    "up": "CREATE TABLE jobs (id INT NOT NULL AUTO_INCREMENT, jobTitle TEXT,description TEXT,companyName VARCHAR(50),status VARCHAR(50),userId INT,createdAt DATETIME NOT NULL,updatedAt DATETIME NOT NULL ,PRIMARY KEY (id))",
+    "up": "CREATE TABLE jobs (id INT NOT NULL AUTO_INCREMENT, jobTitle TEXT,description TEXT,link TEXT,companyName VARCHAR(50),status VARCHAR(50),userId INT,createdAt DATETIME NOT NULL,updatedAt DATETIME NOT NULL ,PRIMARY KEY (id))",
     "down": "DROP TABLE jobs"
 }
