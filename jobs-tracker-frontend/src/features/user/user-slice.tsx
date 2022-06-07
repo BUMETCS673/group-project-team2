20de9bf9d9a9cfe7f7a24ff4c3fd4f5d343fd1e7
Taina Conde <tainabatistaconde@gmail.com>
2022-06-07 17:09:17 -0400

    Create userSlice

---
+++ jobs-tracker-frontend/src/features/user/user-slice.tsx
@@ -0,0 +1,37 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+
+interface UserState {
+    id: number,
+    username : string,
+    name: string,
+    email: string,
+    password: string,
+    createdAt: Date,
+}
+
+const initialState: UserState = {
+    id: 0,
+    username : "",
+    name: "",
+    email: "",
+    password: "",
+    createdAt: new Date(),
+}
+
+const userSlice = createSlice({
+    name: 'user',
+    initialState,
+    reducers: {
+        addUser(state, action: PayloadAction<UserState>) {
+            state.id = action.payload.id
+            state.username = action.payload.username
+            state.name = action.payload.name
+            state.password = action.payload.password
+            state.email = action.payload.email
+            state.createdAt = action.payload.createdAt
+
+        }
+    }
+})
+export const { addUser } = userSlice.actions
+export default userSlice.reducer
