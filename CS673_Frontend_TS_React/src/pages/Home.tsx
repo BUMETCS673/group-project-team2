b6b60acd73c713d43f9b2b8522e05d175ba06ee1
Chinmay337 <65412787+Chinmay337@users.noreply.github.com>
2022-06-21 01:44:58 -0400

    Added Demo Video explaining all functionality

--- CS673_Frontend_TS_React/src/pages/Home.tsx
+++ CS673_Frontend_TS_React/src/pages/Home.tsx
@@ -1,42 +1,38 @@
-import React, {useEffect} from 'react'
+import React, { useEffect } from 'react'
 import { StyledCardsContainer } from '../styles/styles'
 import HomePageHeader from '../components/home/HomePageHeader'
 import { useAuth0 } from '@auth0/auth0-react'
-import {useAppDispatch, useAppSelector} from '../redux/app/hooks'
+import { useAppDispatch, useAppSelector } from '../redux/app/hooks'
 import { setUserToken } from '../redux/slices/model/user'
 import JobCardList from '../components/job/JobCardList'
-
 
 export const Home = () => {
   const { user, isAuthenticated, getAccessTokenSilently } = useAuth0()
   const dispatch = useAppDispatch()
-  const userToken = useAppSelector(state => state.user.token)
-  
-  useEffect(()=> {
+  const userToken = useAppSelector((state) => state.user.token)
+
+  useEffect(() => {
     if (isAuthenticated && user) {
-    const getClaims = async () => {
-      const claims = await getAccessTokenSilently()
-      
-      dispatch(setUserToken(claims))
+      const getClaims = async () => {
+        const claims = await getAccessTokenSilently()
+
+        dispatch(setUserToken(claims))
+      }
+      getClaims()
     }
-    getClaims()}
   }, [isAuthenticated, dispatch])
-  
-  
-  
-   
+
   return (
-    <StyledCardsContainer >
-      {userToken.length != 0  && (
+    <StyledCardsContainer>
+      {userToken.length != 0 && (
         <>
           <HomePageHeader />
           <br />
           <br />
           <br />
-          <JobCardList/>
+          <JobCardList />
         </>
       )}
     </StyledCardsContainer>
   )
-
 }
